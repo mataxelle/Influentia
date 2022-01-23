776f1737c1082ab8a208f4c9a3e4c69e09ac677e
axelle <kadance9720@gmail.com>
2022-01-23 13:47:32 +0100

    Add relation between entity into test

--- tests/PostUtilTest.php
+++ tests/PostUtilTest.php
@@ -2,7 +2,10 @@
 
 namespace App\Tests;
 
+use App\Entity\Category;
 use App\Entity\Post;
+use App\Entity\User;
+use DateTime;
 use PHPUnit\Framework\TestCase;
 
 class PostUtilTest extends TestCase
@@ -10,31 +13,53 @@ class PostUtilTest extends TestCase
     public function testTrue(): void
     {
         $post = new Post();
+        $user = new User();
+        $datetime = new DateTime();
+        $categories = new Category;
 
         $post->setTitle('title')
              ->setContent('content')
              ->setAuthor('author')
-             ->setImage('image');
+             ->setUser($user)
+             ->setImage('image')
+             ->setPublicationDate($datetime)
+             ->setUpdateDate($datetime)
+             ->addCategory($categories);
 
         $this->assertTrue($post->getTitle() === 'title');
         $this->assertTrue($post->getContent() === 'content');
         $this->assertTrue($post->getAuthor() === 'author');
+        $this->assertTrue($post->getUser() === $user);
         $this->assertTrue($post->getImage() === 'image');
+        $this->assertTrue($post->getPublicationDate() === $datetime);
+        $this->assertTrue($post->getUpdateDate() === $datetime);
+        $this->assertContains($categories, $post->getCategories());
     }
 
     public function testFalse(): void
     {
         $post = new Post();
+        $user = new User();
+        $datetime = new DateTime();
+        $categories = new Category;
 
         $post->setTitle('title')
              ->setContent('content')
              ->setAuthor('author')
-             ->setImage('image');
+             ->setUser($user)
+             ->setImage('image')
+             ->setPublicationDate($datetime)
+             ->setUpdateDate($datetime)
+             ->addCategory($categories);
 
         $this->assertFalse($post->getTitle() === 'false');
         $this->assertFalse($post->getContent() === 'false');
         $this->assertFalse($post->getAuthor() === 'false');
+        $this->assertFalse($post->getUser() === new User());
         $this->assertFalse($post->getImage() === 'false');
+        $this->assertFalse($post->getPublicationDate() === new Datetime);
+        $this->assertFalse($post->getUpdateDate() === new Datetime);
+        $this->assertNotContains(new Category(), $post->getCategories());
     }
 
     public function testEmpty(): void
@@ -44,6 +69,10 @@ class PostUtilTest extends TestCase
         $this->assertEmpty($post->getTitle());
         $this->assertEmpty($post->getContent());
         $this->assertEmpty($post->getAuthor());
+        $this->assertEmpty($post->getUser());
         $this->assertEmpty($post->getImage());
+        $this->assertEmpty($post->getPublicationDate());
+        $this->assertEmpty($post->getUpdateDate());
+        $this->assertEmpty($post->getCategories());
     }
 }
--- tests/PreniumPostUtilTest.php
+++ tests/PreniumPostUtilTest.php
@@ -2,7 +2,10 @@
 
 namespace App\Tests;
 
+use App\Entity\Category;
 use App\Entity\PreniumPost;
+use App\Entity\User;
+use DateTime;
 use PHPUnit\Framework\TestCase;
 
 class PreniumPostUtilTest extends TestCase
@@ -10,31 +13,53 @@ class PreniumPostUtilTest extends TestCase
     public function testTrue(): void
     {
         $preniumPost = new PreniumPost();
+        $user = new User();
+        $datetime = new DateTime();
+        $categories = new Category;
 
         $preniumPost->setTitle('title')
              ->setContent('content')
              ->setAuthor('author')
-             ->setImage('image');
+             ->setUser($user)
+             ->setImage('image')
+             ->setPublicationDate($datetime)
+             ->setUpdateDate($datetime)
+             ->addCategory($categories);
 
         $this->assertTrue($preniumPost->getTitle() === 'title');
         $this->assertTrue($preniumPost->getContent() === 'content');
         $this->assertTrue($preniumPost->getAuthor() === 'author');
+        $this->assertTrue($preniumPost->getUser() === $user);
         $this->assertTrue($preniumPost->getImage() === 'image');
+        $this->assertTrue($preniumPost->getPublicationDate() === $datetime);
+        $this->assertTrue($preniumPost->getUpdateDate() === $datetime);
+        $this->assertContains($categories, $preniumPost->getCategories());
     }
 
     public function testFalse(): void
     {
         $preniumPost = new PreniumPost();
+        $user = new User();
+        $datetime = new DateTime();
+        $categories = new Category;
 
         $preniumPost->setTitle('title')
              ->setContent('content')
              ->setAuthor('author')
-             ->setImage('image');
+             ->setUser($user)
+             ->setImage('image')
+             ->setPublicationDate($datetime)
+             ->setUpdateDate($datetime)
+             ->addCategory($categories);
 
         $this->assertFalse($preniumPost->getTitle() === 'false');
         $this->assertFalse($preniumPost->getContent() === 'false');
         $this->assertFalse($preniumPost->getAuthor() === 'false');
+        $this->assertFalse($preniumPost->getUser() === new User());
         $this->assertFalse($preniumPost->getImage() === 'false');
+        $this->assertFalse($preniumPost->getPublicationDate() === new Datetime);
+        $this->assertFalse($preniumPost->getUpdateDate() === new Datetime);
+        $this->assertNotContains(new Category(), $preniumPost->getCategories());
     }
 
     public function testEmpty(): void
@@ -44,6 +69,10 @@ class PreniumPostUtilTest extends TestCase
         $this->assertEmpty($preniumPost->getTitle());
         $this->assertEmpty($preniumPost->getContent());
         $this->assertEmpty($preniumPost->getAuthor());
+        $this->assertEmpty($preniumPost->getUser());
         $this->assertEmpty($preniumPost->getImage());
+        $this->assertEmpty($preniumPost->getPublicationDate());
+        $this->assertEmpty($preniumPost->getUpdateDate());
+        $this->assertEmpty($preniumPost->getCategories());
     }
 }
